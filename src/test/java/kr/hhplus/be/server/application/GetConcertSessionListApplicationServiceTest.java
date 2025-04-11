077455a934f90f61af12055a6713ff5c62e78f87
dongwooLee <dleh684@gmail.com>
2025-04-11 09:04:46 +0900

    test: GetConcertSessionListApplicationServiceTest 수정, 콘서트 회차 조회 예외 처리 테스트 추가

--- src/test/java/kr/hhplus/be/server/application/GetConcertSessionListApplicationServiceTest.java
+++ src/test/java/kr/hhplus/be/server/application/GetConcertSessionListApplicationServiceTest.java
@@ -1,8 +1,7 @@
 package kr.hhplus.be.server.application;
 
-import kr.hhplus.be.server.domain.Concert;
-import kr.hhplus.be.server.domain.ConcertRepository;
-import kr.hhplus.be.server.domain.ConcertSession;
+import kr.hhplus.be.server.domain.*;
+import kr.hhplus.be.server.exception.ConcertNotFoundException;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -16,6 +15,7 @@ import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
+import static org.junit.Assert.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.when;
 
@@ -34,20 +34,24 @@ class GetConcertSessionListApplicationServiceTest {
         // given
         UUID concertId = UUID.randomUUID();
 
+        List<Seat> seats = List.of(new Seat(UUID.randomUUID(), concertId, 1L, "VIP", 10000, SeatStatus.AVAILABLE));
+
         List<ConcertSession> mockSessions = List.of(
                 new ConcertSession(
                         UUID.randomUUID(),
                         concertId,
                         LocalDateTime.of(2025, 4, 20, 16, 0),
                         LocalDateTime.of(2025, 4, 20, 20, 0),
-                        50
+                        50,
+                        seats
                 ),
                 new ConcertSession(
                         UUID.randomUUID(),
                         concertId,
                         LocalDateTime.of(2025, 4, 21, 16, 0),
                         LocalDateTime.of(2025, 4, 21, 20, 0),
-                        50
+                        50,
+                        seats
                 )
         );
 
@@ -70,6 +74,19 @@ class GetConcertSessionListApplicationServiceTest {
         // then
         assertEquals(2, result.size());
         assertEquals(50, result.get(0).availableSeats());
+    }
+
+    @Test
+    @DisplayName("콘서트가 존재하지 않으면 예외가 발생한다")
+    void getConcertSessionList_ThrowsException_WhenConcertNotFound() {
+        // given
+        UUID concertId = UUID.randomUUID();
+
+        // when
+        when(concertRepository.findById(concertId)).thenReturn(Optional.empty());
+
+        // then
+        assertThrows(ConcertNotFoundException.class, () -> getConcertSessionListService.getConcertSessionList(concertId));
     }
 }
 
