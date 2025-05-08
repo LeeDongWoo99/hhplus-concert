840c039505d0a38e372adf2120058a94d9eb0d34
dongwooLee <dleh684@gmail.com>
2025-05-08 15:20:37 +0900

    test: Redis 통합 테스트를 위한 Testcontainers 구성

--- src/test/java/io/hhplus/concert/TestcontainersConfiguration.java
+++ src/test/java/io/hhplus/concert/TestcontainersConfiguration.java
@@ -3,34 +3,53 @@ package io.hhplus.concert;
 import jakarta.annotation.PreDestroy;
 
 import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Configuration;
+import org.testcontainers.containers.GenericContainer;
 import org.testcontainers.containers.MySQLContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.utility.DockerImageName;
 
-@TestConfiguration
+import java.time.Duration;
+
+@Configuration
 public class TestcontainersConfiguration {
 
-	@Container
-	public static final MySQLContainer<?> MYSQL_CONTAINER = new MySQLContainer<>(DockerImageName.parse("mysql:8.0"))
-		.withDatabaseName("hhplus")
-			.withUsername("test")
-			.withPassword("test");
+	public static final MySQLContainer<?> MYSQL_CONTAINER;
+	public static final GenericContainer<?> REDIS_CONTAINER;
 
-	static  {
+	static {
+		// MySQL 컨테이너 설정 및 실행
+		MYSQL_CONTAINER = new MySQLContainer<>(DockerImageName.parse("mysql:8.0"))
+				.withDatabaseName("hhplus")
+				.withUsername("test")
+				.withPassword("test");
 		MYSQL_CONTAINER.start();
 
+		// Redis 컨테이너 설정 및 실행
+		REDIS_CONTAINER = new GenericContainer<>(DockerImageName.parse("redis:7.2.3"))
+				.withExposedPorts(6379)
+				.withStartupTimeout(Duration.ofSeconds(30));
+		REDIS_CONTAINER.start();
+
+		// MySQL 설정 - Spring Application에 주입
 		System.setProperty("spring.datasource.url", MYSQL_CONTAINER.getJdbcUrl() + "?characterEncoding=UTF-8&serverTimezone=UTC");
 		System.setProperty("spring.datasource.username", MYSQL_CONTAINER.getUsername());
 		System.setProperty("spring.datasource.password", MYSQL_CONTAINER.getPassword());
-		System.setProperty("spring.jpa.hibernate.ddl-auto", "create");
-		System.setProperty("spring.jpa.show-sql", "true");
-		System.setProperty("spring.jpa.properties.hibernate.format-sql", "true");
+
+		// Redis 설정 - Spring Application에 주입
+		System.setProperty("spring.redis.host", REDIS_CONTAINER.getHost());
+		System.setProperty("spring.redis.port", REDIS_CONTAINER.getMappedPort(6379).toString());
 	}
 
+	// 테스트가 끝나면 컨테이너 종료
 	@PreDestroy
 	public void preDestroy() {
-		if ( MYSQL_CONTAINER.isRunning() ) {
+		if (MYSQL_CONTAINER.isRunning()) {
 			MYSQL_CONTAINER.stop();
+		}
+		if (REDIS_CONTAINER.isRunning()) {
+			REDIS_CONTAINER.stop();
 		}
 	}
 }
+
