6f76d0ca3235025520aeb101c62de8032137fca8
dongwooLee <dleh684@gmail.com>
2025-05-08 15:18:43 +0900

    feat: RedissonClient 설정 및 빈 등록

---
+++ src/main/java/io/hhplus/concert/domain/reservation/RedissonConfig.java
@@ -0,0 +1,23 @@
+package io.hhplus.concert.domain.reservation;
+
+import lombok.RequiredArgsConstructor;
+import org.redisson.Redisson;
+import org.redisson.api.RedissonClient;
+import org.redisson.config.Config;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@RequiredArgsConstructor
+public class RedissonConfig {
+
+    private final RedisProperties redisProperties;
+
+    @Bean(destroyMethod = "shutdown")
+    public RedissonClient redissonClient() {
+        Config config = new Config();
+        config.useSingleServer()
+                .setAddress("redis://" + redisProperties.getHost() + ":" + redisProperties.getPort());
+        return Redisson.create(config);
+    }
+}
