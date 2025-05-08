9fb1eaecb112878e66be67e6b66af8654d4658ce
dongwooLee <dleh684@gmail.com>
2025-05-08 15:17:05 +0900

    feat: Redis 설정 및 프로퍼티 클래스 추가

---
+++ src/main/java/io/hhplus/concert/domain/reservation/RedisProperties.java
@@ -0,0 +1,16 @@
+package io.hhplus.concert.domain.reservation;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@ConfigurationProperties(prefix = "spring.redis")
+@Component
+@Getter
+@Setter
+public class RedisProperties {
+    private String host;
+    private int port;
+}
+
